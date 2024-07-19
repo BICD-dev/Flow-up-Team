2a65a8ddd0a6e1d496da6d08d6af1dd3508d70e5
Bright Ihegawam <bright06iheagwam@gmail.com>
2024-07-19 16:00:31 +0100

    still building

---
+++ script.js
@@ -0,0 +1,8 @@
+//sidebar script
+let open = document.querySelector(".openSidebar");
+let sidebar = document.querySelector(".sidebar");
+open.addEventListener("click",()=>{
+    sidebar.classList.toggle("close");
+
+})
+//intersection observer
